ad886f7b441bda972f906922cff440ae3714610e
hanhpham327 <hanhtp2001@gmail.com>
2018-05-10 12:50:53 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# MERN Stack Setup Demo
+
+## Setup Instructions
+
+> 1. For this repository then clone to your machine
+> 1. `npm install` Server dependencies
+> 1. `npm install` Client dependencies
+> 1. `npm run dev` View in browser
---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const PORT = process.envPORT || 9000;
+const app=express();
+app.get('/', (req,res) =>{
+    res.send('<h1> App is working!! </h1>')
+})
+
+app.listen (PORT, ()=> {
+    console.log('Server running at localhost:' + PORT);
+}).on('error',(err)=>{
+    console.log('Server Error: ' , err.message);
+    console.log('Do you already have a server running on PORT: ' +PORT);
+});
+
