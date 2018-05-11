908d9962257662a774baed331169fbb6b1ac8f9a
hanhpham327 <hanhtp2001@gmail.com>
2018-05-10 17:46:15 -0700

    set up for deployment

--- client/src/components/test.js
+++ client/src/components/test.js
@@ -3,9 +3,27 @@ import axios from 'axios';
 class Test extends Component {
     componentDidMount(){
         this.getUserData();
+        this.getArticle();
+        this.sendData();
+
     }
+    async getArticle(){
+        const response = await axios.get('/api/get-article');
+        console.log("Article Data: ",response);
+    }
+
+    async sendData(){
+        const dataToSend = {
+            something:"Here is something",
+            anotherthing:"Here is another thing",
+            stuff:"What is all this then?"
+        }
+        const response = await axios.post('/api/send-data',dataToSend);
+        console.log("Send Data: ",response)
+    }
+
     async getUserData(){
-        const response= await axios.get('http://localhost:9000/user-data');
+        const response= await axios.get('/api/user-data');
         // we have to put await in front of axios
         //don't forget a sync in front of the function
         console.log('User Data Response:  ', response )
--- index.js
+++ index.js
@@ -1,20 +1,39 @@
 const express = require('express');
 const cors = require('cors');
+const {resolve}=require('path');
 const PORT = process.envPORT || 9000;
 const app=express();
 
 app.use(cors());
+app.use(express.static(resolve(__dirname,'client','dist')));
+// app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.post('/api/send-data',(req,res)=>{
+    console.log('Data sent: ', req.body);
+    res.send({
+        success:true,
+         mirror: req.body
+    })
+});
 
-app.get('/user-data',(req,res)=>{
+
+app.get('/api/user-data',(req,res)=>{
     const user={
         name:'Jim Bob',
         email:'jimthebob@mail.com'
     }
     res.send(user);
 });
-
-app.get('/', (req,res) =>{
-    res.send('<h1> HOT POCKETS </h1>')
+app.get('/api/get-article',(req,res)=>{
+    const article={
+        title:'Some Awesome Article',
+        author: "John Walker",
+        content:"Lorem ipsum dolor sit, amet consecteur apdiciadof akerujeka ed kdsfjwer"
+    };
+    res.send(article);
+})
+app.get('*',(req,res)=>{
+    res.sendFile(resolve(__dirname,'client','dist','index.html'))
 })
 
 app.listen (PORT, ()=> {
